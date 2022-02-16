d0d169995de267abea116616db0e58f0ce3daa68
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-17 01:05:21 +0600

    NoteApp

---
+++ NotesApp/NotesApp/Controllers/NoteController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NotesApp.Data;
+using NotesApp.Models;
+using NotesApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesApp.Controllers
+{
+    public class NoteController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NoteController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Note> objList = _db.Notes;
+
+            foreach (var obj in objList)
+            {
+                obj.NoteType = _db.NoteTypes.FirstOrDefault(u => u.Id == obj.NoteTypeId);
+            }
+
+
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+
+            NoteVM noteVM = new NoteVM()
+            {
+                Note = new Note(),
+                TypeDropDown = _db.NoteTypes.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            return View(noteVM);
+        }
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(NoteVM obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Notes.Add(obj.Note);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var noteDetails = _db.Notes.Find(id);
+            noteDetails.ReadCount += 1;
+            _db.SaveChanges();
+
+            IEnumerable<Note> objList = _db.Notes;
+
+            foreach (var obj in objList)
+            {
+                obj.NoteType = _db.NoteTypes.FirstOrDefault(u => u.Id == obj.NoteTypeId);
+            }
+
+
+            if (noteDetails == null)
+            {
+                return View("Not Found");
+            }
+
+            return View(noteDetails);
+        }
+
+
+        //GET Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Notes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Note obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Notes.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ NotesApp/NotesApp/Models/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesApp.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(100000)]
+        public string Content { get; set; }
+        public bool Important { get; set; } = false;
+        public int ReadCount { get; set; } = 0;
+        public string Creator { get; set; } = "Default User";
+
+        [DisplayName("Note Type")]
+        public int NoteTypeId { get; set; }
+        [ForeignKey("NoteTypeId")]
+        public virtual NoteType NoteType { get; set; }
+    }
+}
